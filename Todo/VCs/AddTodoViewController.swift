d9bd5402abe46fb46f3e5ad0e83f2ccb053e72f1
TonyTang2001 <tonytangdev@outlook.com>
2018-07-28 00:24:21 +0800

    UI: + Now AddTodo VC has Transparent Background & Dissolves into Current View.

--- Todo/VCs/AddTodoViewController.swift
+++ Todo/VCs/AddTodoViewController.swift
@@ -18,22 +18,44 @@ class AddTodoViewController: UIViewController {
         return textField
     }()
     
+    let cancelButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = UIColor.lightGray
+        button.setTitle("Cancel", for: .normal)
+        button.setTitleColor(UIColor.black, for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 6
+        button.layer.masksToBounds = true
+        button.addTarget(self, action: #selector(cancel), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc func cancel() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.addSubview(eventNameTF)
+        view.addSubview(cancelButton)
         
-        
-//        eventNameTFSetup()
-        
+        eventNameTFSetup()
+        cancelButtonSetup()
     }
     
     func eventNameTFSetup() {
         eventNameTF.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         eventNameTF.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         eventNameTF.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -48).isActive = true
-        eventNameTF.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        eventNameTF.heightAnchor.constraint(equalToConstant: 30).isActive = true
     }
 
+    func cancelButtonSetup() {
+        cancelButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        cancelButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 40).isActive = true
+    }
 
 }
